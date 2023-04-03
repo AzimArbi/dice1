071dbb637bec9e73776835fee1eee9ee2e0869ac
AzimArbi <127857537+AzimArbi@users.noreply.github.com>
2023-04-03 17:10:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImages = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "images" + randomDiceImages;
+var randomImageSource = "images/" + randomDiceImages;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
